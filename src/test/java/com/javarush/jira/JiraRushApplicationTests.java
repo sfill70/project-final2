cc836bc7595faa0f8d69bf333af9a949b5647967
sfill70 <sergfill70@yandex.ru>
2023-05-25 14:25:56 +0300

    4.Redo the tests so that during the tests the in memory database (H2)

--- src/test/java/com/javarush/jira/JiraRushApplicationTests.java
+++ src/test/java/com/javarush/jira/JiraRushApplicationTests.java
@@ -3,6 +3,7 @@ package com.javarush.jira;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class JiraRushApplicationTests {
 	@Test
